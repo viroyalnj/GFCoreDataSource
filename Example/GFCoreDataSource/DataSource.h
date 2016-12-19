7673bf45381efc6bc2eaf3520a61c3f6723bdfe3
guofengld <guofengld@gmail.com>
2016-12-19 21:33:40 +0800

    update

---
+++ Example/GFCoreDataSource/DataSource.h
@@ -0,0 +1,14 @@
+//
+//  DataSource.h
+//  GFCoreDataSource
+//
+//  Created by 熊国锋 on 2016/12/19.
+//  Copyright © 2016年 guofengld. All rights reserved.
+//
+
+#import "BaseDataModal.h"
+#import "BoxItemEntity.h"
+
+@interface DataSource : BaseDataModal
+
+@end
---
+++ Example/GFCoreDataSource/DataSource.m
@@ -0,0 +1,13 @@
+//
+//  DataSource.m
+//  GFCoreDataSource
+//
+//  Created by 熊国锋 on 2016/12/19.
+//  Copyright © 2016年 guofengld. All rights reserved.
+//
+
+#import "DataSource.h"
+
+@implementation DataSource
+
+@end
