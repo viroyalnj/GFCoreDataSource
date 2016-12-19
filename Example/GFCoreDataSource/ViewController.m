a72155abc2ca2c4d1ca5b2d975b81da548c6f9ff
guofengld <guofengld@gmail.com>
2016-12-19 20:24:01 +0800

    update

--- Example/GFCoreDataSource/ViewController.m
+++ Example/GFCoreDataSource/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "BaseDataModal.h"
 
 @interface ViewController ()
 
--- Example/Pods/Target Support Files/GFCoreDataSource/GFCoreDataSource-umbrella.h
+++ Example/Pods/Target Support Files/GFCoreDataSource/GFCoreDataSource-umbrella.h
@@ -2,6 +2,8 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "BaseDataModal.h"
+#import "ObjectProcessor.h"
 
 FOUNDATION_EXPORT double GFCoreDataSourceVersionNumber;
 FOUNDATION_EXPORT const unsigned char GFCoreDataSourceVersionString[];
