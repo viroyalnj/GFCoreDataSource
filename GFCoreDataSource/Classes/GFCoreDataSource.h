d65a3214c2075a4e09fb42c525f93183f3af6e0b
guofengld <guofengld@gmail.com>
2016-12-29 20:01:10 +0800

    GFCoreDataSource.h

---
+++ GFCoreDataSource/Classes/GFCoreDataSource.h
@@ -0,0 +1,16 @@
+//
+//  GFCoreDataSource.h
+//  Pods
+//
+//  Created by 熊国锋 on 2016/12/29.
+//
+//
+
+#ifndef GFCoreDataSource_h
+#define GFCoreDataSource_h
+
+#import "GFObjectOperation.h"
+#import "GFDataSource.h"
+
+
+#endif /* GFCoreDataSource_h */
