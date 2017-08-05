69507aea63e8cd933d845d2cabc82e984084bd9e
xgf_nj <xgf_nj@viroyal-elec.com>
2017-08-05 16:23:25 +0800

    gitignore

--- Example/Pods/Target Support Files/GFCoreDataSource/GFCoreDataSource-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GFCoreDataSource : NSObject
-@end
-@implementation PodsDummy_GFCoreDataSource
-@end
--- Example/Pods/Target Support Files/Pods-GFCoreDataSource_Tests/Pods-GFCoreDataSource_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GFCoreDataSource_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_GFCoreDataSource_Tests
-@end
